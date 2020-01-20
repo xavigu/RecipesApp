f08c47ad8ec027a368986cbbe06fee43f3bd1315
fj_babel <francisco.guzman@babel.es>
2020-01-20 17:54:51 +0100

    Create ingredient model and use it in shopping-list component

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient {
+    //Poniendo public en los argumentos del constructor es una forma abreviada de definirte primero las variables y asignarle los valores de los argumentos del constructor 
+    constructor(public name: string, public amount:number){}
+    
+
+}
