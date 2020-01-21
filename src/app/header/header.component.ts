9f072146d051ae0b5d2079886264ffe2c74bccb2
fj_babel <francisco.guzman@babel.es>
2020-01-21 16:45:06 +0100

    Add funcionality in the header using EventEmitter binding and ngIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() featureSelected = new EventEmitter<string>();
+
   collapsed = true;
   
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
   }
 
 }
