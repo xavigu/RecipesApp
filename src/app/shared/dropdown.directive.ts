1c5e8074e47c2f69da48021534a386fca4e609bf
fj_babel <francisco.guzman@babel.es>
2020-01-22 15:57:14 +0100

    Change directive to close dropdown when you click outside of the dropdown(not only in dropdown element)

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -4,11 +4,9 @@ import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core'
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-
-  @HostBinding('class.open') isOpen:boolean = false;
-
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
-
+  constructor(private elRef: ElementRef) { }
 }
