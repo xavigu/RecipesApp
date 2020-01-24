e3f2de27b63102f66d5c775b78e6c272dc808ded
fj_babel <francisco.guzman@babel.es>
2020-01-24 14:58:02 +0100

    Subscribe to show if in the params there is and id or not to change the value of the boolean variable

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor() { }
+  id:number;
+  editMode = false;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+        .subscribe((params:Params)=>{
+           this.id = +params['id'];
+           this.editMode = params['id'] != null; //Si id no es undefined el editMode estaría a true
+           console.log(this.editMode);
+        })
   }
 
 }
