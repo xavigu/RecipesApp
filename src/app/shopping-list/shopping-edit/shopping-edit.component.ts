ec4eb90fdac07e2b88703b06b6d8a8851ddd44ff
fj_babel <francisco.guzman@babel.es>
2020-01-28 10:12:49 +0100

    Add getIngredient method in the service to pick the Ingredient object and setValue to the ngForm when you clicked

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
@@ -10,9 +10,11 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('f', {static: false}) slForm: NgForm;
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private shopService: ShoppingListService) { }
 
@@ -21,6 +23,11 @@ export class ShoppingEditComponent implements OnInit {
         .subscribe((index: number) => {
           this.editedItemIndex = index;
           this.editMode = true;
+          this.editedItem = this.shopService.getIngredient(index);
+          this.slForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          });
         });
   }
 
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -18,6 +18,10 @@ export class ShoppingListService {
 
   constructor() { }
 
+  getIngredient(index: number){
+    return this.ingredients[index];
+  }
+
   getIngredients(){
     return this.ingredients.slice();
   }
