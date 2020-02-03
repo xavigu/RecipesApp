4a0bc3850d5feca9561aec505516a425d4950c26
fj_babel <francisco.guzman@babel.es>
2020-02-03 17:48:07 +0100

    Store and overwrite all the recipes with put request in the firebase dtabase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
 
 
 @Injectable({
     providedIn: 'root'
 })
-export class DataStorageService {  
-    constructor(private http: HttpClientModule) { }
+export class DataStorageService {
+    constructor(private http: HttpClient, private recipeService: RecipeService) { }
+    //Nos subscribimos directamente en el metodo que hace la put request porque no estamos interesados en la respuesta
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        return this.http.put('https://ng-course-recipeapp.firebaseio.com/recipes.json', recipes)
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
 
 }
