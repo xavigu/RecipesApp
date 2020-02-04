4df04cfe50abf7695b03689e644121c605d03e94
fj_babel <francisco.guzman@babel.es>
2020-02-04 14:00:04 +0100

    Fix bug in the resolver to can show the changed when you edit a recipe

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -3,15 +3,22 @@ import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/r
 
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from './recipe.service';
 
 @Injectable({providedIn: 'root'})
 //utiliza la interface Resolve y hay que definir el tipo de data que va a resolver
 export class RecipesResolverService implements Resolve<Recipe[]> {
-    constructor(private dataStorageService: DataStorageService){}
+    constructor(private dataStorageService: DataStorageService, private recipesService: RecipeService){}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot ){
-        return this.dataStorageService.fetchRecipes();
+        const recipes = this.recipesService.getRecipes();
+        //solo se subscribe al fetchRecipes que hace el get del backend
+        //si el array de las Recipes esta a 0 porque no se ha fetcheado la data aun
+        if (recipes.length === 0) {
+            return this.dataStorageService.fetchRecipes();
+        } else {
+            return recipes;
+        }       
     }
-
 }
 
