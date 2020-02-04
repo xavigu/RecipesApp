115e79a2065620adb7693a1d2b5c49d436205e59
fj_babel <francisco.guzman@babel.es>
2020-02-04 13:50:49 +0100

    Add return in fetchRecipes to subscribe in the header component and use in the resolve to use this resolve in the paths that you need the data

---
+++ src/app/recipes/recipes-resolver.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { Recipe } from './recipe.model';
+import { DataStorageService } from '../shared/data-storage.service';
+
+@Injectable({providedIn: 'root'})
+//utiliza la interface Resolve y hay que definir el tipo de data que va a resolver
+export class RecipesResolverService implements Resolve<Recipe[]> {
+    constructor(private dataStorageService: DataStorageService){}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot ){
+        return this.dataStorageService.fetchRecipes();
+    }
+
+}
+
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map } from 'rxjs/operators'
+import { map, tap } from 'rxjs/operators'
 
 
 @Injectable({
@@ -20,16 +20,20 @@ export class DataStorageService {
     };
     //Realizamos el subscribe porque la respuesta puede interesar al recipeService que es que maneja las recipes
     fetchRecipes() {
-        this.http.get<Recipe[]>('https://ng-course-recipeapp.firebaseio.com/recipes.json')
+       return this.http.get<Recipe[]>('https://ng-course-recipeapp.firebaseio.com/recipes.json')
             .pipe(map(recipes => {
                 return recipes.map(recipe => { //JS Method para mapear el array Recipes y modificar su contenido
                     return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
                     //el spread operator(...) es para copiar toda la data existente y mira si existe el recipe.ingredients y si no crea para esa variable un empty array
                 });
-            }))
-            .subscribe((recipes: Recipe[]) => {
-                this.recipeService.setRecipes(recipes);
-            })
+            }),
+             tap(recipes =>{
+                 this.recipeService.setRecipes(recipes);
+             })  //tap operator permite ejecutar un codigo sin alterar la data que esta siendo funneled a traves del observable
+            )
     }
-
 }
+// .subscribe((recipes: Recipe[]) => {
+//     this.recipeService.setRecipes(recipes);
+// })
+
