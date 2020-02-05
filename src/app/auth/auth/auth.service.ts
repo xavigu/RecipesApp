3f5d1c9ed71995f5cd93728081d9cac15dc80299
fj_babel <francisco.guzman@babel.es>
2020-02-05 13:16:57 +0100

    Share error handling between signup and login creating and handleError method

--- src/app/auth/auth/auth.service.ts
+++ src/app/auth/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
@@ -29,26 +29,8 @@ export class AuthService {
       email: email,
       password: password,
       returnSecureToken: true
-    }).pipe(catchError(errorResp => {
-      let errorMessage = 'An unknown error occured';
-      if (!errorResp.error || !errorResp.error.error) {
-        return throwError(errorMessage);
-      }
-      switch (errorResp.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'The email address is already in use by another account.'
-          break;
-        case 'OPERATION_NOT_ALLOWED':
-          errorMessage = 'Password sign-in is disabled for this project.'
-          break;
-        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-          errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.'
-          break;
-        default:
-          break;
-        }
-        return throwError(errorMessage); 
-    }));
+    }).pipe(catchError(this.handleError));
+
   };
 
   login(email: string, password: string){
@@ -57,6 +39,38 @@ export class AuthService {
       email: email,
       password: password,
       returnSecureToken: true
-    })
+    }).pipe(catchError(this.handleError));
+  }
+
+  //method that return throwError and use it in the catchError operator
+  private handleError(errorRes: HttpErrorResponse){
+    let errorMessage = 'An unknown error occured';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'The email address is already in use by another account.'
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Password sign-in is disabled for this project.'
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.'
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.'
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password is invalid or the user does not have a password.'
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'The user account has been disabled by an administrator.'
+        break;
+      default:
+        break;
+    }
+    return throwError(errorMessage); 
+
   }
 }
