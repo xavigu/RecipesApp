b170a74c652599f57ead16e0b62717413b5f9b10
fj_babel <francisco.guzman@babel.es>
2020-02-07 12:44:54 +0100

    Add core module to declarate there all the core services of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AuthComponent } from './auth/auth/auth.component';
-import { RecipeService } from './recipes/recipe.service';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { AuthInterceptorService } from './auth/auth/auth-interceptor.service';
+
 
 @NgModule({
   declarations: [
@@ -29,16 +28,9 @@ import { AuthInterceptorService } from './auth/auth/auth-interceptor.service';
     ReactiveFormsModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [RecipeService,
-              ShoppingListService,
-              {
-                provide: HTTP_INTERCEPTORS,
-                useClass: AuthInterceptorService,
-                multi: true
-              }
-             ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { RecipeService } from './recipes/recipe.service';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AuthInterceptorService } from './auth/auth/auth-interceptor.service';
+
+@NgModule({
+    providers: [
+        RecipeService,
+        ShoppingListService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptorService,
+            multi: true
+        }
+    ]
+})
+
+export class CoreModule { }
