f8b599d68699e7d607cf903c2c843b8a8f5ed430
fj_babel <francisco.guzman@babel.es>
2020-02-07 11:39:03 +0100

    Split routing module creating a new shopping-list routing module to all shopping-list paths and import in shopping-list.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { AuthComponent } from './auth/auth/auth.component';
 
 
 const appRoutes: Routes = [  
   { path: '', redirectTo: '/recipes', pathMatch: 'full' }, //Te redirija a /recipes si el path entero es vacio
-  { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'auth', component: AuthComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { PlaceholderDirective } from './shared/placeholder.directive';
+import { AlertComponent } from './shared/alert/alert.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+
+import { AuthComponent } from './auth/auth/auth.component';
 import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { AppRoutingModule } from './app-routing.module';
-import { AuthComponent } from './auth/auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
-import { PlaceholderDirective } from './shared/placeholder.directive';
+
 import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
     PlaceholderDirective,
     AuthComponent,
@@ -37,7 +35,8 @@ import { RecipesModule } from './recipes/recipes.module';
     HttpClientModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    RecipesModule
+    RecipesModule,
+    ShoppingListModule
   ],
   providers: [RecipeService,
               ShoppingListService,
