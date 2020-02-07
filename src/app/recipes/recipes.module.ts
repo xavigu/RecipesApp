f189f4af31a914d1b84a7e39dad460522b5c4f7b
fj_babel <francisco.guzman@babel.es>
2020-02-07 11:05:48 +0100

    Split routing module creating a new recipes routing module to all recipes paths and import in recipes.module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { RecipesComponent } from './recipes.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from '../app-routing.module';
-import { CommonModule } from '@angular/common';
+import { RecipesRoutingModule } from './recipes-routing.module';
 
 @NgModule({
     declarations: [
@@ -19,9 +21,10 @@ import { CommonModule } from '@angular/common';
         RecipeEditComponent
     ],
     imports: [
+        RouterModule,
         CommonModule, //Similar al BrowserModule que solo se puede utilizar en el app.module
         ReactiveFormsModule,
-        AppRoutingModule
+        RecipesRoutingModule
     ],
     exports: [
         RecipesComponent,
