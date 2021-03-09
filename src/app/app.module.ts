c50fa92fc338caafc12f10d6256def0893430ec4
xavigu <xavigu91@gmail.com>
2021-03-09 18:28:07 +0100

    Provide the shoppingListReducer like a StoreModule to all app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { StoreModule } from '@ngrx/store'
 
-import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core.module';
+import { AppRoutingModule } from './app-routing.module'
+import { SharedModule } from './shared/shared.module'
+import { CoreModule } from './core.module'
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './header/header.component'
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    StoreModule.forRoot({ shoppingList: shoppingListReducer }),
     BrowserAnimationsModule,
     SharedModule,
-    CoreModule
+    CoreModule,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
