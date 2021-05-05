bcc6885b370c4271ef145f4f6d1c27d404f6db6b
xavigu <xavigu91@gmail.com>
2021-05-05 13:24:54 +0200

    Add translateModule to shopping-list lazyload module and multilanguage to the component

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -6,19 +6,25 @@ import { ShoppingListRoutingModule } from './shopping-list.routing.module';
 
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list.component';
-
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
-    declarations: [
-        ShoppingListComponent,
-        ShoppingEditComponent
-    ],
-    imports: [
-        CommonModule,
-        FormsModule,
-        ShoppingListRoutingModule,
-        SharedModule
-    ]
+  declarations: [ShoppingListComponent, ShoppingEditComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ShoppingListRoutingModule,
+    SharedModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => new TranslateHttpLoader(http),
+        deps: [HttpClient],
+      },
+      extend: true,
+    }),
+  ],
 })
-
 export class ShoppingListModule {}
